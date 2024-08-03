8dc4a9e18da93d5d83e39b0429a83d60d4243190
Gerardo Munguía Hoyo <gerardo.munguia.hoyo@gmail.com>
2024-08-03 17:30:29 +0200

    add gitops stack

---
+++ stacks/gitops/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 4000;
+const secret = process.env.GITHUB_WEBHOOK_SECRET;
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  const payload = req.body;
+  console.log('Received webhook:', payload);
+  res.status(200).send('Webhook received');
+});
+
+app.listen(port, () => {
+  console.log(`Webhook listener app is listening on port ${port}`);
+});
