4d8285933133766699a2477a1871d90474e3f9cd
Gerardo Munguía Hoyo <gerardo.munguia.hoyo@gmail.com>
2024-08-04 09:01:56 +0200

    fix signature validation

--- bootstrap/gitops/webhook/index.js
+++ bootstrap/gitops/webhook/index.js
@@ -56,19 +56,52 @@ app.use(
   }),
 );
 
-const verifySignature = (req) => {
-  const signature = req.headers["x-hub-signature-256"];
-  if (!signature || !GITHUB_WEBHOOK_SECRET) {
-    return false;
+const verifySignature = async (secret, header, payload) => {
+  let encoder = new TextEncoder();
+  let parts = header.split("=");
+  let sigHex = parts[1];
+
+  let algorithm = { name: "HMAC", hash: { name: "SHA-256" } };
+
+  let keyBytes = encoder.encode(secret);
+  let extractable = false;
+  let key = await crypto.subtle.importKey(
+    "raw",
+    keyBytes,
+    algorithm,
+    extractable,
+    ["sign", "verify"],
+  );
+
+  let sigBytes = hexToBytes(sigHex);
+  let dataBytes = encoder.encode(payload);
+  let equal = await crypto.subtle.verify(
+    algorithm.name,
+    key,
+    sigBytes,
+    dataBytes,
+  );
+
+  return equal;
+};
+
+const hexToBytes = (hex) => {
+  let len = hex.length / 2;
+  let bytes = new Uint8Array(len);
+
+  let index = 0;
+  for (let i = 0; i < hex.length; i += 2) {
+    let c = hex.slice(i, i + 2);
+    let b = parseInt(c, 16);
+    bytes[index] = b;
+    index += 1;
   }
-  const hmac = crypto.createHmac("sha256", GITHUB_WEBHOOK_SECRET);
-  hmac.update(req.rawBody);
-  const digest = `sha256=${hmac.digest("hex")}`;
-  return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest));
+
+  return bytes;
 };
 
 app.post("/webhook", (req, res) => {
-  if (!verifySignature(req)) {
+  if (!verifySignature(GITHUB_WEBHOOK_SECRET, req.headers["x-hub-signature-256"], req.body)) {
     logger.warn("Invalid signature");
     return res.status(403).send("Invalid signature");
   }
