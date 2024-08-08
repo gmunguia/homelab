0b23b3f964cdec29f0d13619eb40ec19674d08be
Gerardo Munguía Hoyo <gerardo.munguia.hoyo@gmail.com>
2024-08-08 15:10:14 +0200

    allow cloudflare token to be passed as env var

--- bootstrap/gitops/tunnel/entrypoint.sh
+++ bootstrap/gitops/tunnel/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+set -e
 
-TOKEN=$(cat "$CLOUDFLARE_TOKEN_SECRET_FILE")
+if [ -n "$CLOUDFLARE_TOKEN" ]; then
+	TOKEN="$CLOUDFLARE_TOKEN"
+elif [ -n "$CLOUDFLARE_TOKEN_SECRET_FILE" ] && [ -f "$CLOUDFLARE_TOKEN_SECRET_FILE" ]; then
+	TOKEN=$(cat "$CLOUDFLARE_TOKEN_SECRET_FILE")
+else
+	echo "Error: Neither CLOUDFLARE_TOKEN is set nor CLOUDFLARE_TOKEN_SECRET_FILE is set and valid." >&2
+	exit 1
+fi
 
 exec cloudflared tunnel --no-autoupdate run --token "$TOKEN"
