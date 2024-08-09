73f4e417423771fa54a0d7d617651e89236149ef
Gerardo Munguía Hoyo <gerardo.munguia.hoyo@gmail.com>
2024-08-09 22:20:23 +0200

    add graceful shutdown

--- bootstrap/gitops/webhook/index.js
+++ bootstrap/gitops/webhook/index.js
@@ -16,15 +16,52 @@ process.on("unhandledRejection", (reason) => {
   process.exit(1);
 });
 
-app.listen(config.PORT, () => {
+const server = app.listen(config.PORT, () => {
   logger.info(
     { event: "server-start", port: config.PORT },
     `Server is listening on port ${config.PORT}`,
   );
 });
 
-(async () => {
-  while (true) {
-    await processQueue();
-  }
+const shutdownQueue = (() => {
+  let processing;
+  let isShuttingDown = false;
+
+  (async () => {
+    while (!isShuttingDown) {
+      processing = processQueue();
+      await processing;
+    }
+  })();
+
+  return async () => {
+    isShuttingDown = true;
+    await processing;
+  };
 })();
+
+const shutdownServer = async () => {
+  return new Promise((resolve) => {
+    server.close((err) => {
+      if (err) {
+        logger.error(
+          { event: "server-close-error", err },
+          "Error closing server",
+        );
+      }
+      resolve();
+    });
+  });
+};
+
+["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) => {
+  process.on(signal, async () => {
+    logger.info({ event: "shutdown-initiated", signal }, "Shutdown initiated");
+
+    await shutdownServer();
+    await shutdownQueue();
+
+    logger.info({ event: "shutdown-complete" }, "Shutdown complete");
+    process.exit(0);
+  });
+});
