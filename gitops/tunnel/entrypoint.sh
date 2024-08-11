2f5525718f87594e6c8690a676ff623d50770a61
Gerardo Munguía Hoyo <gerardo.munguia.hoyo@gmail.com>
2024-08-11 10:26:23 +0200

    remove faulty bash conditional

--- gitops/tunnel/entrypoint.sh
+++ gitops/tunnel/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -n "$CLOUDFLARE_TOKEN" ]; then
 	TOKEN="$CLOUDFLARE_TOKEN"
-elif [ -n "$CLOUDFLARE_TOKEN_SECRET_FILE" ] && [ -f "$CLOUDFLARE_TOKEN_SECRET_FILE" ]; then
+elif [ -n "$CLOUDFLARE_TOKEN_SECRET_FILE" ]; then
 	TOKEN=$(cat "$CLOUDFLARE_TOKEN_SECRET_FILE")
 else
 	echo "Error: Neither CLOUDFLARE_TOKEN is set nor CLOUDFLARE_TOKEN_SECRET_FILE is set and valid." >&2
