4fc9163720001bea2e4d34ab00cb2e2c1378e35c
Gerardo Munguía Hoyo <gerardo.munguia.hoyo@gmail.com>
2024-08-11 13:27:19 +0200

    add gitops deploy script

--- gitops/deploy.sh
+++ gitops/deploy.sh
@@ -2,20 +2,40 @@
 
 set -e
 
+STACK_NAME="gitops"
+COMPOSE_FILE="docker-compose.yml"
+SERVICES=("webhook" "tunnel")
+
+if ! command -v yq &>/dev/null; then
+	echo "yq is not installed. Please install it to proceed."
+	echo "See https://github.com/mikefarah/yq#install"
+	exit 1
+fi
+
+get_image_name() {
+	local service=$1
+  local remove_image_tag='s/(.+):.+/\1/'
+	yq e ".services.$service.image" $COMPOSE_FILE | sed -E $remove_image_tag
+}
+
 GIT_HASH=$(git rev-parse --short HEAD)
 
-echo "Building Docker images"
-docker-compose build --pull
+echo "Building Docker images with tag: $GIT_HASH"
+for service in "${SERVICES[@]}"; do
+	echo "Building $service"
+	image_name=$(get_image_name "$service")
+	docker build -t "$image_name:latest" -t "$image_name:$GIT_HASH" "$service"
+done
 
-echo "Pushing Docker images with tag: $GIT_HASH"
-for service in $(docker-compose config --services); do
-	image=$(docker-compose config | grep "image:" | grep "$service" | awk '{print $2}')
-	docker tag "$image:latest" "$image:$GIT_HASH"
-	docker push "$image:$GIT_HASH"
+echo "Pushing Docker images"
+for service in "${SERVICES[@]}"; do
+	echo "Pushing $service"
+	image_name=$(get_image_name "$service")
+	docker push "$image_name:latest"
+	docker push "$image_name:$GIT_HASH"
 done
 
 echo "Updating Docker Swarm stack"
-STACK_NAME="gitops"
-docker stack deploy -c docker-compose.yml $STACK_NAME
+docker stack deploy -c $COMPOSE_FILE $STACK_NAME
 
 echo "Deployment completed successfully!"
